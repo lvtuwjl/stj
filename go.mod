3dd9a1bbde01fdb7659118aaab665c0cc524d684
Jian Li <jianli@hk.com>
2022-04-25 19:14:38 +0800

    add

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lvtuwjl/stj
+
+go 1.18
+
+require github.com/spf13/cast v1.4.1
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/spf13/cast"
+)
+
+// func main() {
+// 	fmt.Println(123)
+// }
+
+type D struct {
+	Name string
+	Age  int
+	Addr string
+}
+
+func StructToJson(d *D) string {
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		return ""
+	}
+	var s string
+
+	i := cast.ToInt(s)
+
+	fmt.Println("i value is:", i)
+
+	return string(bytes)
+}
